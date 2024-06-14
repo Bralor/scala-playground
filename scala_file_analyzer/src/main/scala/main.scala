e45abd274b0fa02889a60a3fbbca2b54487ba114
Matouš Holinka <bralor92@email.cz>
2024-06-14 16:12:49 +0200

    main.scala: Fixed the word counting.

--- scala_file_analyzer/src/main/scala/main.scala
+++ scala_file_analyzer/src/main/scala/main.scala
@@ -2,13 +2,27 @@ import scala.io.Source
 import java.nio.file.{Paths, Files}
 
 @main
+/** The project's notes
+ * ---
+ *
+ * Here comes the notes:
+ * 1. Read the TXT file
+ * 2. split the whole string
+ * 3. count the lowercase words
+ */
 def main(): Unit = {
   val testFilePath: String = "src/main/resources/txt_sample.txt"
   val fileContent = readFileContent(filePath = testFilePath)
 
-  fileContent match {
-    case None => println("File does not exist")
-    case Some(text) => println(s"Content of TXT: $text")
+  val wordCounts = for {
+    loadedText <- fileContent
+    words <- splitIntoSingleWords(stringContent = loadedText)
+    wordCounts <- mapAllWordsLength(allWords = words)
+  } yield wordCounts
+
+  wordCounts match {
+    case None => println("Some exceptions has occured.")
+    case Some(wordMap) => for ((key, value) <- wordMap) println(s"$key: $value")
   }
 }
 
@@ -17,5 +31,24 @@ def readFileContent(filePath: String): Option[String] = {
     None
   } else {
     Some(Source.fromFile(filePath).mkString)
+  }
+}
+
+def splitIntoSingleWords(stringContent: String,
+                         separator: String = "\\s+"): Option[Array[String]] = {
+  if (stringContent.isEmpty) {
+    None
+  } else {
+    Some(stringContent.split(separator))
+  }
+}
+
+def mapAllWordsLength(allWords: Array[String]): Option[Map[String, Int]] = {
+  if (allWords.isEmpty) {
+    None
+  } else {
+    Some(allWords.foldLeft(Map.empty[String, Int]) {
+      (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
+    })
   }
 }
